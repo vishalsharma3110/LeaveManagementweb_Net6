142b323e91675602d08b9e4ae5caeefe37f98267
vishalsharma31103110 <vishalsharma3110@gmail.com>
2024-05-04 13:14:28 +0530

    Add project files.

---
+++ LeaveManagementProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
